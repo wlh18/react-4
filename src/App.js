0225b4b02e7525e5e1d64a323abec5702294e87c
tstokes9328 <taytestokes9328@gmail.com>
2019-08-07 15:38:06 -0600

    added swapi example with axios

--- src/App.js
+++ src/App.js
@@ -14,16 +14,36 @@ class App extends Component {
 
   // lifecycle method to run on the mounting of App
   componentDidMount(){
-    // save the promise axios returns
-    const swapiPromise = axios.get('https://swapi.com/api/people');
+    // save the promise that axios will return
+    const swapiPromise = axios.get('https://swapi.co/api/people/');
+    // once we have that promise, execute it
+    swapiPromise.then(response => {
+      console.log(response)
+      // update state with information from the response
+      this.setState({
+        people: response.data.results
+      })
+    })
+    .catch(error => {
+      // use .catch to handle a network error
+      console.log(error)
+    });
 
   }
 
   render(){
-    // map through the people on state to return 
+    // map through the people on state to return jsx elements for each character
+    const mappedCharacters = this.state.people.map((person, index) => {
+      return (
+        <h2 key={index}>{person.name}</h2>
+      )
+    });
+
     return (
       <div>
-
+        <h1>Characters</h1>
+        {/* display the mapped characters */}
+        {mappedCharacters}
       </div>
     )
   }
